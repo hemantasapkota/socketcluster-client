420a046887f8e2d1d9629afede3cfe4a68292483
Hemanta Sapkota <laex.pearl@gmail.com>
2015-10-27 10:50:52 +1100

    Initial Commit

---
+++ swift/socketclusterclient/socketclusterclient/socketclusterclient.h
@@ -0,0 +1,19 @@
+//
+//  socketclusterclient.h
+//  socketclusterclient
+//
+//  Created by Hemanta Sapkota on 27/10/2015.
+//  Copyright (c) 2015 Hemanta Sapkota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for socketclusterclient.
+FOUNDATION_EXPORT double socketclusterclientVersionNumber;
+
+//! Project version string for socketclusterclient.
+FOUNDATION_EXPORT const unsigned char socketclusterclientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <socketclusterclient/PublicHeader.h>
+
+
