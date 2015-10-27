ea84fd6fe8cb8cad7f7e2240af62ffab43a26434
Hemanta Sapkota <laex.pearl@gmail.com>
2015-10-27 14:50:01 +1100

    Scaffold for example

---
+++ swift/socketclusterclient/example/example/example/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  example
+//
+//  Created by Hemanta Sapkota on 27/10/2015.
+//  Copyright (c) 2015 Hemanta Sapkota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var hostTxt: UITextField!
+    @IBOutlet weak var secureSwitch: UISwitch!
+    @IBOutlet weak var connectBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
